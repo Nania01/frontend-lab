a2531ba5df4661aa368bdbe317fd899957303345
Nania01 <148682556+Nania01@users.noreply.github.com>
2025-10-31 17:08:04 +1000

    move project to Nuxt and split layout into components

---
+++ my-nuxt-app/app/components/Banner.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="banner">Banner</div>
+</template>
+
+<style lang="scss">
+@use '@/assets/styles/mixins' as *;
+
+.banner {
+  @include card(green, white);
+}
+</style>
---
+++ my-nuxt-app/nuxt.config.ts
@@ -0,0 +1,11 @@
+export default defineNuxtConfig({
+  modules: [
+    ['@nuxtjs/google-fonts', {
+      families: {
+        'Open Sans': [400, 700],
+        Montserrat: true,
+      }
+    }]
+  ],
+  compatibilityDate: '2025-10-31'
+})
